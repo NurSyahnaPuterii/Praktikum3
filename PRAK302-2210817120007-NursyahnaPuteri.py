c2f7804ed8599891c060504297ea5c1483ec9949
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-25 07:48:28 +0800

    first commit

---
+++ PRAK302-2210817120007-NursyahnaPuteri.py
@@ -0,0 +1,12 @@
+nilai = int (input ("Masukan Nilai : "))
+
+if nilai >=80 :
+    print ("predikit Nilai A ")
+elif nilai >=70 and nilai <=79:
+    print ("Predikat Nilai B ")
+elif nilai  >=60 and nilai <=69:
+    print ("Predikat NIlai C ")
+elif nilai >=50 and nilai <=59:
+    print ("Predikit Nilai D")
+else :
+    print ("Predikat Nilai E")
